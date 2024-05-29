739e1c8a5cbd06a38407241e79ed8a467f560ee7
Juan Yepes <yepes.j@hotmail.com>
2024-05-28 20:32:12 -0400

    fix: apostrophe lint err

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -10,7 +10,9 @@ export function Footer() {
     <div className="pt-[80px] pb-[40px]">
       <div className="flex items-center justify-center gap-x-[12px]">
         <Image src={Logo} alt="Logo" />
-        <p className="font-bold text-[#36485C] text-[17px]">Little Bambi's</p>
+        <p className="font-bold text-[#36485C] text-[17px]">
+          Little Bambi&apos;s
+        </p>
       </div>
 
       <ul className="flex flex-col items-center gap-y-[32px] pt-[56px] text-[#36485C] sm:flex-row sm:justify-center sm:gap-x-5 sm:pt-5">
@@ -21,7 +23,7 @@ export function Footer() {
       </ul>
 
       <p className="pt-[56px] text-center text-[14px] font-medium text-[#5F7896] sm:pt-5">
-        © Copyright 2024. Little Bambi's at Play. All rights reserved.
+        © Copyright 2024. Little Bambi&apos;s at Play. All rights reserved.
       </p>
 
       <div className="flex items-center justify-center gap-x-[56px] pt-[40px]">
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -7,7 +7,7 @@ export function Hero() {
     <div className="pt-4 lg:pt-10">
       <div className="px-[20px] lg:px-[280px]">
         <h1 className="text-center text-[32px] leading-[40px] font-medium text-[#172026] lg:text-[64px] lg:leading-[72px]">
-          Little Bambi's at Play!
+          Little Bambi&apos;s at Play!
         </h1>
         <p className="text-center pt-6 text-[#36485C] lg:text-[18px] lg:leading-7">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
